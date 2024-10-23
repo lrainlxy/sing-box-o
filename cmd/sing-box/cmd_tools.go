6e8e810e46a3a3cdde2655c9ff6b2bcf4b6a686e
世界 <i@sekai.icu>
2024-10-23 13:44:08 +0800

    Implement dns-hijack

--- cmd/sing-box/cmd_tools.go
+++ cmd/sing-box/cmd_tools.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"github.com/sagernet/sing-box"
 	E "github.com/sagernet/sing/common/exceptions"
 	N "github.com/sagernet/sing/common/network"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -23,7 +25,9 @@ func init() {
 func createPreStartedClient() (*box.Box, error) {
 	options, err := readConfigAndMerge()
 	if err != nil {
-		return nil, err
+		if !(errors.Is(err, os.ErrNotExist) && len(configDirectories) == 0 && len(configPaths) == 1) || configPaths[0] != "config.json" {
+			return nil, err
+		}
 	}
 	instance, err := box.New(box.Options{Options: options})
 	if err != nil {
